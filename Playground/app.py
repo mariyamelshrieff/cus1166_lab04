035fa61cbf91ce79c8ab92406b0d714fa43859f6
MariyamElshrief <mariyam.elshrief17@stjohns.edu>
2019-09-25 08:02:57 -0400

    Updated app.py

--- Playground/app.py
+++ Playground/app.py
@@ -5,3 +5,16 @@ app = flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/welcome/ <string:student_name>")
+def welcome (student_name):
+    return render_template("welcome.html", student_name= student_name)
+
+
+@app.route("/roster/<int:grade_view>")
+def class_roster (grade_view):
+    class_roster= [("John","A", "Sophomore"),
+    ("Tom","B","Junior"), ("Sam","C","Senior"), ("Arly","D","Freshman"), ("Stevie","A", "Freshman")
+    ("Sandra","A","Junior"), ("Maya","B","Senior"),("Mike","C","Senior"), ("Holly","A","Freshman")]
+
+    return render_template ("roster.html",student_name=student_name,class_roster=class_roster)
