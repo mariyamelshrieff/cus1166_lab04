4f1f627ff89be4321a548716fb4775724117eaeb
MariyamElshrief <mariyam.elshrief17@stjohns.edu>
2019-10-02 13:47:42 -0400

    Updated roster

--- Playground/app.py
+++ Playground/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask import render_template
-import templates
+
+
 
 app = Flask(__name__)
+#Bootstrap(app)
 
 class_roster = [("Sam" , "A", "Sophomore"),
                 ("Stevie","B", "Freshman"),
-                ("Arly","C","Senior"),
+                ("Arly","B","Senior"),
                 ("Bodan", "A", "Sophomore"),
-                ("Mike", "B","Freshman"),
-                ("Lauren","D","Senior"),
+                ("Mike", "C","Freshman"),
+                ("Lauren","C","Senior"),
                 ("Joseph","A","Senior")]
 
 @app.route('/')
@@ -22,10 +24,10 @@ def welcome(student_name):
     return render_template("welcome.html", student_name=student_name)
 
 
-@app.route('/roster/<int:grade_view>/')
-def roster(grade_roster):
+@app.route('/roster/<string:grade_view>/')
+def roster(grade_view):
 
-    return render_template("roster.html", class_roster= class_roster, grade_view=grade_view)
-
+    return render_template("roster.html", class_roster = class_roster, grade_view = grade_view)
+#For Running the app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=true)
